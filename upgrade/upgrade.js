0ff6275d8ff909ff423cfd7da3e671378154b528
Andrew McOlash <amcolash@gmail.com>
2018-11-30 22:11:44 -0800

    Webhook upgrades

---
+++ upgrade/upgrade.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { spawn } = require('child_process');
+
+// Get env vars
+require('dotenv').config();
+const UPGRADE_KEY = process.env.UPGRADE_KEY;
+
+// Make server
+const PORT = 9001;
+const app = express();
+app.listen(PORT);
+console.log(`Running on port ${PORT}`);
+
+// Handle upgrade
+app.get('/upgrade', function (req, res) {
+    if (req.query.upgradeKey === UPGRADE_KEY) {
+        const proc = spawn('sh', ['upgrade.sh'], { cwd: './' });
+        proc.stdout.on('data', data => console.log(data.toString()));
+        proc.stderr.on('data', data => console.error(data.toString()));
+        proc.on('exit', code => {
+            console.log("upgrade exited with code " + code);
+            res.send("all done!");
+        });
+    } else {
+        res.send("invalid upgrade key!");
+    }
+});
