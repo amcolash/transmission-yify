f9d196643696d8b86bfd71eaeb14fb478180b6f3
Andrew McOlash <amcolash@gmail.com>
2019-01-21 16:01:43 -0800

    Fater startup for ip service

--- get_ip.sh
+++ get_ip.sh
@@ -1,10 +1,8 @@
 echo Starting IP service
 touch /data/ip.txt
 
-# Wait for vpn to start up
-sleep 20
-
 # Every minute check our ip address, write to shared file
+# NOTE: The first ip address will be incorrect since the vpn service has not started
 while true
 do 
     curl -s http://ipinfo.io/ip > /data/ip.txt 
