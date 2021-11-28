798b0d0fdb5ff6cceab3d753fa3967f3301f22fd
Andrew McOlash <amcolash@gmail.com>
2021-11-28 16:53:28 -0600

    add piano led proxy

--- server/index.js
+++ server/index.js
@@ -62,6 +62,7 @@ app.use(redirectToHTTPS());
 // proxy remote commands through
 app.use('/remote', proxy(process.env.REMOTEBOOT_IP));
 app.use('/spectrum', proxy(process.env.SPECTRUM_IP));
+app.use('/piano-led', proxy(process.env.PIANO_LED_IP));
 
 // record requests through analytics handler
 app.use(analyticsMiddleware);
